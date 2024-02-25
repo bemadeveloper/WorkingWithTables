e2b1e8396c07976470f9fdbb91c9eabc06021467
bemadeveloper <akbermet1990@icloud.com>
2024-02-25 12:36:34 +0600

    Initial Commit

---
+++ WorkingWithTables/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkingWithTables
+//
+//  Created by Bema on 25/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
